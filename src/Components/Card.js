ae079e1cd99e0d798ba8b25bed22b6d647af185f
PatilDivyani <125538181+PatilDivyani@users.noreply.github.com>
2023-07-20 20:34:52 +0530

    Add files via upload

---
+++ src/Components/Card.js
@@ -0,0 +1,41 @@
+import React from "react";
+
+let dayList = [
+  {
+    place: [ "Ramkund",'Panchvati',
+        'Sita Gufaa',
+        'Sri Saptshrungi Devi Temple',
+        'Sri Kalaram Swamy Temple'],
+  },
+  {
+    place: ['Muktidham',
+        'Buddha Leni Caves',
+        'Godheshwar Temple'
+        ],
+  },
+  {
+    place: ['Dugarwadi Waterfall',
+        'Trimbakeshwar Temple',
+        'Anjereri Mountain',
+       ' Bramagiri Hill'],
+  },
+  {
+    place: ['Someshwar Temple'],
+  },
+
+];
+ 
+const Card = () => {
+  return <>
+  {
+    // dayList.map((item)=>{
+    //   <ul>
+    //     item.place.map((place)=><li>{place} </li>)
+    //   </ul>
+      
+   // })
+  }
+  </>;
+};
+
+export default Card;
